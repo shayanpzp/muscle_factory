9beab80dd23701d21201019c3eb5c3f9b439b13a
shayanpzp <shayanpzp1@gmail.com>
2025-12-06 21:48:03 +0330

    end test for DEBUG

--- muscle_factory/settings.py
+++ muscle_factory/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uxaz+c2er2=ymp0ubu^8#b2=it*ad$cg)2j&8vgm1z)4(erbz7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 
