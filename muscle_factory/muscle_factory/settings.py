598b8e91a432866528cf32c366d5d3698bf7d450
shayanpzp <shayanpzp1@gmail.com>
2025-12-06 20:09:44 +0330

    change stng for host

--- muscle_factory/muscle_factory/settings.py
+++ muscle_factory/muscle_factory/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uxaz+c2er2=ymp0ubu^8#b2=it*ad$cg)2j&8vgm1z)4(erbz7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
